8b0d36c673bee5948ca5f3cb3e139452818b8c6e
Fardin Haque <fardinhaque60@gmail.com>
2024-09-26 21:55:22 -0700

    established a few routes and refactor root

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'create-account', component: CreateAccountComponent},
+];
