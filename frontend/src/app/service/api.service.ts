78f76fd4883dd2c232dc5ca1a07b00454c1b6b20
Fardin Haque <fardinhaque60@gmail.com>
2024-09-26 21:55:39 -0700

    api service injectable

---
+++ frontend/src/app/service/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+/**
+ * Service for making API GET and POST requests
+ * components can import ApiService and use its getRequest and postRequest methods
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl: string = "http://localhost:8000/";
+
+  constructor(private http: HttpClient) { }
+
+  getRequest(endpoint: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${endpoint}`);
+  }
+
+  postRequest(endpoint: string, payload: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/${endpoint}`, payload);
+  }
+}
