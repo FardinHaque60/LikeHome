250eef940520631e73688f5cabd12ec875ede54d
Fardin Haque <fardinhaque60@gmail.com>
2024-10-11 16:47:00 -0700

    add spinner to search results

--- frontend/src/app/search-results/search-results.component.ts
+++ frontend/src/app/search-results/search-results.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../service/api.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss'
 })
 export class SearchResultsComponent implements OnInit {
   searchResults: Array<any> = [];
   searchParams: any;
+  loading: boolean = true;
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
@@ -24,6 +26,7 @@ export class SearchResultsComponent implements OnInit {
             console.log(response['status']);
             console.log(response['hotels']);
             this.searchResults = response['hotels'];
+            this.loading = false;
           },
           error: (error) => {
             console.log("Search Error");
