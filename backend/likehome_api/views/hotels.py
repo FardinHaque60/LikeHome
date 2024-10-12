41f7323559ffffe6c6bfc2de3a8e4f3c70394828
Fardin Haque <fardinhaque60@gmail.com>
2024-10-11 21:49:54 -0700

    handle frontend data

--- backend/likehome_api/views/hotels.py
+++ backend/likehome_api/views/hotels.py
@@ -10,16 +10,17 @@ from .clients import *
 @api_view(['POST'])
 def search(request):
     data = request.data
-    location, check_in, check_out, rooms, adults = data['location'], data['check_in'], data['check_out'], data['rooms'], data['adults']
+    location, check_in, check_out, rooms, adults, children = data['location'], data['check_in'], data['check_out'], data['rooms'], data['adults'], data['children']
 
-    # TODO apply some processor on check_in/ out dates to conform to input format
-    print(location, check_in, check_out, rooms, adults)
+    rooms, children, adults = int(rooms), int(children), int(adults)
+    print(location, check_in, check_out, rooms, adults, children)
     
-    # response = hotel_availability(location=location, check_in="2024-12-01", check_out="2024-12-02", adults=2, children=2, rooms=1)
+    # response = hotel_availability(location=location, check_in=check_in, check_out=check_out, adults=adults, children=children, rooms=rooms)
     with open("playground/hotel_availability.json", "r") as file:
         response = json.load(file)
+    
     try:
-        if response.status_code != 200:
+        if response['status_code'] != 200:
             return Response({'status': 'Error', 'message': 'Error getting hotel details'}, status=status.HTTP_400_BAD_REQUEST)
     except: # if response is valid then it has no status_code, thus valid
         pass
