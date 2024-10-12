68b5aaec1d7f3c001519789afa684755da686e07
noahnguyen015 <n.noah3355@gmail.com>
2024-10-12 01:51:33 -0700

    linking added between checkout and hotel detail page, navigation testing

--- frontend/src/app/hotel-details/hotel-details.component.ts
+++ frontend/src/app/hotel-details/hotel-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
@@ -13,11 +13,17 @@ import { FooterComponent } from '../shared/footer/footer.component';
 })
 export class HotelDetailsComponent implements OnInit{
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
 
   ngOnInit(): void {
-    
+        //moves to the top of the page when finished navigating (back&forth)
+        //seems to not work with <-- back arrows
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                window.scrollTo(0, 0)
+            }
+        });
   }
 
 }
