738ab50551dbadc12aaac1266c56479e079d1f0c
Fardin Haque <fardinhaque60@gmail.com>
2024-10-11 20:46:55 -0700

    blocked hotel api calls

--- backend/likehome_api/views/hotels.py
+++ backend/likehome_api/views/hotels.py
@@ -15,14 +15,13 @@ def search(request):
     # TODO apply some processor on check_in/ out dates to conform to input format
     print(location, check_in, check_out, rooms, adults)
     
-    # response = hotel_availability(location=location, check_in=check_in, check_out=check_out, adults=adults, children=0, rooms=rooms)
-    response = hotel_availability(location=location, check_in="2024-12-01", check_out="2024-12-02", adults=2, children=2, rooms=1)
-    # hotel_details(int(response['hotels']['hotels'][0]['code']))
-    # hotel_details(252751)
+    # response = hotel_availability(location=location, check_in="2024-12-01", check_out="2024-12-02", adults=2, children=2, rooms=1)
+    with open("playground/hotel_availability.json", "r") as file:
+        response = json.load(file)
     try:
         if response.status_code != 200:
             return Response({'status': 'Error', 'message': 'Error getting hotel details'}, status=status.HTTP_400_BAD_REQUEST)
-    except:
+    except: # if response is valid then it has no status_code, thus valid
         pass
     print("done")
     
