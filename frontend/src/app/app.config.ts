f97ee3971255ae46feddd61e7350edbc7e347f48
noahnguyen015 <n.noah3355@gmail.com>
2024-10-17 12:30:14 -0700

    layout made

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
