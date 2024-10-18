e1f4c537fb76455ba32128b894f212a4cc4bce76
noahnguyen015 <n.noah3355@gmail.com>
2024-10-18 00:43:53 -0700

    populated page with account info and rooms

--- frontend/src/app/account-details/account-details.component.ts
+++ frontend/src/app/account-details/account-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink,ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -8,14 +9,15 @@ import { ApiService } from '../service/api.service';
 @Component({
   selector: 'app-account-details',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FooterComponent, MatProgressSpinnerModule],
+  imports: [RouterLink, NavbarComponent, FooterComponent, 
+            MatProgressSpinnerModule, CommonModule,],
   templateUrl:'./account-details.component.html',
   styleUrl: './account-details.component.scss'
 })
 export class AccountDetailsComponent implements OnInit {
-  constructor(private apiService: ApiService, private router: Router) {
+  constructor(private apiService: ApiService, private router: Router, private activatedroute: ActivatedRoute) {
     this.router.events.subscribe(() => {
-      
+
     });
   }
 
@@ -26,9 +28,11 @@ export class AccountDetailsComponent implements OnInit {
     'username': '',
   };
 
+  bookedRooms: Array<any> = []
+
   
   ngOnInit(): void {
-    /*
+    
     this.apiService.getBackendRequest('get-session')
       .subscribe({
         next: (response) => {
@@ -38,6 +42,31 @@ export class AccountDetailsComponent implements OnInit {
         error: (error) => { 
           console.log(error);
         }
-      });*/
+      })
+
+      this.apiService.getBackendRequest('get-reservations')
+      .subscribe({
+        next: (response) => {
+          console.log(response); // look in inspect element console to see what format data is in for parsing
+          this.bookedRooms = response['reservations'];
+          console.log(this.bookedRooms);
+        },
+        error: (error) => { 
+          console.log(error);
+        }
+      });
+  }
+
+  openHotel(i: number): void {
+    this.router.navigate(['/hotel-details'], { queryParams: { 
+      checkIn: "2024-10-15", 
+      checkOut: "2024-10-18", 
+      details: JSON.stringify(this.bookedRooms[i]) 
+      } 
+    });
+  }
+
+  replaceImage(event: any) {
+    event.target.src = 'assets/images/nexus_logo.png';
   }
 }
