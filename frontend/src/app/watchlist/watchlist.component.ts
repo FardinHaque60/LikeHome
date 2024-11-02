5b44e6f40f9d420c3d518d58577ad0455c5421a1
noahnguyen015 <n.noah3355@gmail.com>
2024-11-01 18:04:56 -0700

    displaying hotels in watchlist

--- frontend/src/app/watchlist/watchlist.component.ts
+++ frontend/src/app/watchlist/watchlist.component.ts
@@ -1,21 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink,ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { ApiService } from '../service/api.service';
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.scss'
 })
 export class WatchlistComponent implements OnInit {
 
+  watchlistHotels: Array<any> = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router, private activatedroute: ActivatedRoute) {
+    this.router.events.subscribe(() => {
 
-  ngOnInit(): void {}
+    });
+  }
+
+  ngOnInit(): void {
+    this.apiService.getBackendRequest('get-watchlist')
+    .subscribe({
+      next: (data: any) => {
+        console.log(data); // look in inspect element console to see what format data is in for parsing
+        this.watchlistHotels = data;
+        console.log(this.watchlistHotels[0].images);
+      },
+      error: (error: any) => {
+        console.log(error);
+      }
+    });
+  }
 
 
   
