5c84cb20564471e7db610ea007ab0ec7dbd8313b
Fardin Haque <fardinhaque60@gmail.com>
2024-11-03 18:05:57 -0800

    open watchlist items

--- frontend/src/app/watchlist/watchlist.component.ts
+++ frontend/src/app/watchlist/watchlist.component.ts
@@ -8,7 +8,6 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 
-
 @Component({
   selector: 'app-homepage',
   standalone: true,
@@ -18,68 +17,57 @@ import { FooterComponent } from '../shared/footer/footer.component';
   styleUrl: './watchlist.component.scss'
 })
 export class WatchlistComponent implements OnInit {
-
   watchlistHotels: Array<any> = [];
 
   loading: boolean = false;
-  searchFilter = new FormGroup({
+  // TODO modify this so it just one field and the values are in html
+  sortFilter = new FormGroup({
     location: new FormControl('', Validators.required),
-    check_in: new FormControl<Date | null>(null),
-    check_out: new FormControl<Date | null>(null),
-    rooms: new FormControl('', Validators.required),
-    adults: new FormControl('', Validators.required),
-    children: new FormControl('', Validators.required),
-    radius: new FormControl('', Validators.required),
-    min_rate: new FormControl('', Validators.required),
-    max_rate: new FormControl('', Validators.required)
+    price: new FormControl('', Validators.required),
+    hotelName: new FormControl('', Validators.required),
+    checkIn: new FormControl('', Validators.required),
+    checkOut: new FormControl('', Validators.required),
   });
 
-  constructor(private apiService: ApiService, private router: Router, private activatedroute: ActivatedRoute) {
-    this.router.events.subscribe(() => {
+  constructor(private apiService: ApiService, private router: Router, private activatedroute: ActivatedRoute) {}
 
-    });
-  }
-
-  getResults(): void {
+  getWatchlist(): void {
     this.apiService.getBackendRequest('get-watchlist')
     .subscribe({
       next: (data: any) => {
         console.log(data); // look in inspect element console to see what format data is in for parsing
         this.watchlistHotels = data;
-        this.loading = false;
       },
       error: (error: any) => {
         console.log('Watchlist Results Error')
         console.log(error);
-        this.loading = false;
       }
     });
   }
 
-    // invoked when search is called from search page itself
-    searchFilterSubmit(): void {
-      this.loading = true;
-      console.log('Search Filter Submitted:', this.searchFilter.value);
-      this.apiService.postBackendRequest('search', this.searchFilter.value)
-          .subscribe({
-            next: (response) => {
-              console.log("Search Request Ack Success");
-              console.log(response);
-              this.getResults();
-            },
-            error: (error) => {
-              console.log("Search Error");
-              console.log(error);
-              this.loading = false;
-            }
-          })
-    }
-
-  ngOnInit(): void {
-    this.getResults();
+  // TODO backend request to sort hotels, maybe just do this browser side
+  sortFilterSubmit(): void {
+    this.loading = true;
+    console.log('Sort Filter Submitted:', this.sortFilter.value);
+    this.loading = false;
   }
 
+  seeHotelDetails(index: number): void {
+    let selectedHotel = this.watchlistHotels[index];
+    selectedHotel['minRate'] = selectedHotel['min_rate'];
+    selectedHotel['maxRate'] = selectedHotel['max_rate'];
+    selectedHotel['name'] = selectedHotel['hotel_name'];
+    this.router.navigate(['/hotel-details'], { 
+      queryParams: 
+        { 
+          checkIn: selectedHotel['check_in'], 
+          checkOut: selectedHotel['check_out'], 
+          details: JSON.stringify(selectedHotel) 
+        } 
+    });
+  }
 
-  
-
+  ngOnInit(): void {
+    this.getWatchlist();
+  }
 }
